be25f798385f8afa01d8083e9378145680f20f39
gilsunshine <gilsunshine@gmail.com>
2018-02-12 10:26:16 -0500

    added solution

--- solution.rb
+++ solution.rb
@@ -1,1 +1,94 @@
 # Please copy/paste all three classes into this file to submit your solution!
+class Customer
+
+  @@all = []
+
+  attr_accessor :first_name, :last_name
+
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name  = last_name
+    @@all << self
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(full_name)
+    self.all.find{|customer| customer.full_name == full_name}
+  end
+
+  def self.find_all_by_first_name(first_name)
+    self.all.select{|customer| customer.first_name == first_name}
+  end
+
+  def self.all_names
+    self.all.map{|customer| customer.full_name}
+  end
+
+  def add_review(restaurant, content)
+    Review.new(self, restaurant, content)
+  end
+
+end
+
+class Review
+
+  @@all = []
+
+  attr_accessor :customer, :restaurant, :content
+
+  def initialize(customer, restaurant, content)
+    @customer = customer
+    @restaurant = restaurant
+    @content = content
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def customer
+    @customer
+  end
+
+  def restaurant
+    @restaurant
+  end
+
+end
+
+class Restaurant
+
+  @@all = []
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    self.all.find{|restaurant| restaurant.name == name}
+  end
+
+  def reviews
+    Review.all.select{|review| review.restaurant == self}
+  end
+
+  def customers
+    self.reviews.map{|review| review.customer}.uniq
+  end
+
+end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,22 @@ def reload
   load 'config/environment.rb'
 end
 
+customer1 = Customer.new("First1", "Last1")
+customer2 = Customer.new("First1", "Last2")
+customer3 = Customer.new("First1", "Last3")
+customer4 = Customer.new("First4", "Last4")
+customer5 = Customer.new("First5", "Last5")
+
+restaurant1 = Restaurant.new("Restaurant1")
+restaurant2 = Restaurant.new("Restaurant2")
+restaurant3 = Restaurant.new("Restaurant3")
+restaurant4 = Restaurant.new("Restaurant4")
+restaurant5 = Restaurant.new("Restaurant5")
+
+review1 = Review.new(customer1, restaurant1, "Review1")
+review2 = Review.new(customer2, restaurant1, "Review2")
+review3 = Review.new(customer3, restaurant1, "Review3")
+review4 = Review.new(customer4, restaurant4, "Review4")
+review5 = Review.new(customer5, restaurant5, "Review5")
+
 Pry.start
