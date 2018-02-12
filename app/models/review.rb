6c238ffd3cdcd91f22e86ca8efae09d893b6ed54
harenarium <22226614+harenarium@users.noreply.github.com>
2018-02-12 10:18:29 -0500

    Done.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,15 @@
 class Review
-  
-end
+  attr_accessor :restaurant, :customer, :content
+  @@all = []
 
+  def initialize(restaurant, customer, content)
+    @restaurant = restaurant
+    @customer = customer
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
--- solution.rb
+++ solution.rb
@@ -1,1 +1,92 @@
 # Please copy/paste all three classes into this file to submit your solution!
+class Customer
+  attr_accessor :first_name, :last_name
+  @@all = []
+
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name  = last_name
+    @@all << self
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    all.find{|customer|
+      customer.full_name == name
+    }
+  end
+
+  def self.find_all_by_first_name(name)
+    all.select{|customer|
+      customer.first_name == name
+    }
+  end
+
+  def self.all_names
+    all.map{|customer|
+      customer.full_name
+    }
+  end
+
+  def add_review(restaurant, content)
+    Review.new(restaurant, self, content)
+  end
+end
+
+#########################################
+
+class Review
+  attr_accessor :restaurant, :customer, :content
+  @@all = []
+
+  def initialize(restaurant, customer, content)
+    @restaurant = restaurant
+    @customer = customer
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
+
+#########################################
+
+class Restaurant
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    all.find{|restaurant|
+      restaurant.name == name
+    }
+  end
+
+  def reviews
+    Review.all.select{|review|
+      review.restaurant == self
+    }
+  end
+
+  def customers
+    reviews.map{|review|
+      review.customer
+    }
+  end
+end
