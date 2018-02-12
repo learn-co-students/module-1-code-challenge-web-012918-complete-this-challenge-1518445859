86c93054f20fd4d1eaaf063d0204a6986e8f962f
izzee <ichabon@me.com>
2018-02-12 10:23:00 -0500

    finished test

--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,19 @@ def reload
   load 'config/environment.rb'
 end
 
+
+customer1 = Customer.new("isaac", "chabon")
+customer2 = Customer.new("donald", "trump")
+customer3 = Customer.new("donald", "duck")
+
+restaurant1 = Restaurant.new("McDonalds")
+restaurant2 = Restaurant.new("Pizza")
+
+customer1.add_review(restaurant1, "Gross")
+customer1.add_review(restaurant2, "Yummy")
+customer2.add_review(restaurant1, "Tasty")
+customer3.add_review(restaurant2, "Delish")
+
+
+
 Pry.start
