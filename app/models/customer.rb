6cc26b6799babfe481a89b59a760cd1e26722667
github username <github email address>
2018-02-12 10:25:57 -0500

    submitting test

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,38 @@
 class Customer
   attr_accessor :first_name, :last_name
 
+  @@all = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
-  def full_name
-    "#{first_name} #{last_name}"
+  def self.all
+    @@all
   end
+
+  def self.find_by_name(full_name)
+    split_name = full_name.split(' ')
+    self.all.find{|customer| customer.first_name == split_name[0] && customer.last_name == split_name[1]}
+  end
+
+  def self.find_by_first_name(first_name)
+    self.all.select{|customer| customer.first_name == first_name}
+  end
+
+  def self.all_names
+    full_names = []
+    self.all.map do |customer|
+      full_names << "#{customer.first_name} #{customer.last_name}"
+    end
+    full_names
+  end
+
+  def add_review(restaurant, content)
+    Review.new(restaurant, content, self)
+  end
+
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,26 @@
 class Review
-  
-end
 
+  attr_accessor :restaurant, :customer, :content
+
+  @@all = []
+
+  def initialize(restaurant, content, customer)
+    @restaurant = restaurant
+    @content = content
+    @customer = customer
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def customer
+    self.customer
+  end
+
+  def restaurant
+    self.restaurant
+  end
+
+end
