d5ad024aaad0aab82d81009660fda566f6ef46e0
github username <github email address>
2018-02-12 10:30:09 -0500

    posting to git

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,43 @@
 class Restaurant
   attr_accessor :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    # given a string of restaurant name, returns the first restaurant that matches
+
+    objects = Restaurant.all.select {|restaurant_obj|
+      restaurant_obj.name == name}
+
+      objects.find {|restaurant_obj| restaurant_obj.name}
+  end
+
+  def reviews
+    # returns an array of all reviews for that restaurant
+    objects = Review.all.select {|review_obj|
+      binding.pry
+      review_obj.restaurant == self}
+
+      objects.map {|review_obj| review_obj.name}
+  end
+
+  def customer
+    # should return all of the customers who have written reviews of that restaurant.
+    objects = Review.all.select {|review_obj|
+      review_obj.restaurant == self}
+
+      objects.map {|review_obj|
+        binding.pry
+        review_obj.full_name}
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,28 @@ def reload
   load 'config/environment.rb'
 end
 
+#CUSTOMERS
+paul_jones = Customer.new("Paul", "Jones")
+paul_stone = Customer.new("Paul", "Stone")
+paul_floor = Customer.new("Paul", "Floor")
+johnny_frost = Customer.new("Johnny", "Frost")
+tom_danza = Customer.new("Tom", "Danza")
+
+###########################################################
+#RESTAURANT
+otto = Restaurant.new("Otto")
+valley = Restaurant.new("Valley")
+freeze = Restaurant.new("Freeze")
+
+###########################################################
+#REVIEWS
+
+paul_jones_otto_review = Review.new(otto, paul_jones)
+tom_danza_valley_review = Review.new(valley, tom_danza)
+johnny_frost_otto_review = Review.new(otto, johnny_frost)
+paul_jones_freeze_review = Review.new(freeze, paul_jones)
+
+
+
+
 Pry.start
