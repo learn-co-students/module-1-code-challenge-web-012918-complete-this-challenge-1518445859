b2975f4aab4932fac6aa29a06352ef95f256b7de
Zahran Kabir <zahran@Zahrans-MacBook-Pro.local>
2018-02-12 10:24:53 -0500

    codechallenge1 zahran

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,17 @@
 class Review
-  
-end
 
+  attr_accessor :restaurant, :content, :customer
+  @@all = []
+
+  def initialize(content, restaurant, customer)
+    @content = content
+    @restaurant = restaurant
+    @customer = customer
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
