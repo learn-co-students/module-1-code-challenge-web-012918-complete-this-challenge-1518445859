5d8f41c8915b8736c23450999998cede002bdbdb
scott <you@example.comitsmescottb123@gmail.com>
2018-02-12 10:16:14 -0500

    Code Challenge 1

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,17 @@
 class Review
-  
-end
+  attr_accessor :customer, :restaurant, :content
 
+  @@all = []
+
+  def initialize(content, customer, restaurant)
+    @customer = customer
+    @restaurant = restaurant
+    @content = content
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
