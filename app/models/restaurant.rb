d8dc812180f88160b03d4b118a2f4f96da2a8cfa
allinthemind66 <r.nilon92@gmail.com>
2018-02-12 10:26:25 -0500

    Submit

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,31 @@
 class Restaurant
   attr_accessor :name
-
+  ALL = []
   def initialize(name)
     @name = name
+    ALL << self
+
   end
 
+  def self.all
+    ALL
+  end
+
+  def self.find_by_name(name)
+    self.all.find do |restaurants|
+      restaurants.name = name
+    end
+  end
+
+  def reviews
+    Review.all.select do |review|
+      review.restaurant.name == self.name
+    end.map {|reviews| reviews.review}
+  end
+
+  def customers
+    Review.all.select do |review|
+      review.restaurant.name == self.name
+    end.map {|reviews| reviews.customer}
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,20 @@
 class Review
-  
-end
+  ALL = []
+  attr_accessor :customer, :restaurant, :review
+  def initialize(customer, restaurant)
+    @customer = customer
+    @restaurant = restaurant
+    ALL << self
+  end
 
+  def self.all
+    ALL
+  end
+
+  def customer
+    @customer
+  end
+  def restaurant
+    @restaurant
+  end
+end
