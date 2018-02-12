fe0e1d52ddd970c1743d5426889475cc3f5b6a83
github username <github email address>
2018-02-12 10:31:05 -0500

    Done.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,28 @@
 class Review
-  
-end
+  attr_accessor :customer, :restaurant, :content
+  @@all = []
 
+  def initialize(customer, restaurant, content)
+    @customer = customer
+    @restaurant = restaurant
+    @content = content
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def customer
+    Review.all.find do |review|
+      if self.content == review
+        review
+      end
+    end
+  end
+
+  def restaurant
+
+  end
+
+end
